78e462cff17b49f1e3e58afd47f74497e8f539b8
Pascal Allau <pascal.allau@zohomail.eu>
2022-09-06 19:04:15 +0200

    [bug report jest] fix bug in src/__tests__/Bills.js

--- src/views/LoginUI.js
+++ src/views/LoginUI.js
@@ -19,9 +19,9 @@ export default () => {
                 <form class="form-signin" data-testid="form-employee">
                   <h2 class="h3 mb-3 font-weight-normal">Employé</h1>
                   <label for="inputEmail">Votre email</label>
-                  <input type="email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
+                  <input type="email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus autocomplete="email">
                   <label for="inputPassword">Mot de passe</label>
-                  <input type="password" data-testid="employee-password-input" class="form-control" placeholder="******" required>
+                  <input type="password" data-testid="employee-password-input" class="form-control" placeholder="******" required autocomplete="current-password">
                   <button class="btn btn-lg btn-primary btn-block" data-testid="employee-login-button" style="background-color: #0E5AE5;" type="submit">Se connecter</button>
                 </form>
               </div>
@@ -33,9 +33,9 @@ export default () => {
                 <form class="form-signin" data-testid="form-admin">
                   <h2 class="h3 mb-3 font-weight-normal">Administration</h1>
                   <label for="inputEmail">Votre email</label>
-                  <input type="email" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
+                  <input type="email" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus autocomplete="email">
                   <label for="inputPassword">Mot de passe</label>
-                  <input type="password" data-testid="admin-password-input" class="form-control" placeholder="******" required>
+                  <input type="password" data-testid="admin-password-input" class="form-control" placeholder="******" required autocomplete="current-password">
                   <button  type="submit" class="btn btn-lg btn-primary btn-block" data-testid="admin-login-button" style="background-color: #0E5AE5;">Se connecter</button>
                 </form>
               </div>
@@ -45,4 +45,3 @@ export default () => {
       </div>
   `)
 }
-  
