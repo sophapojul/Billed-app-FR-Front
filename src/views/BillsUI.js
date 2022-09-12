24fc40655f235fb6d2a2d68dc3f8d10a481142f9
Pascal Allau <pascal.allau@zohomail.eu>
2022-09-12 16:10:13 +0200

    test(views):add sort function to bills in billsUI of views

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,11 +20,16 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
+    if (data && data.length) {
+        data.sort((a, b) => {
+            return new Date(b.date) - new Date(a.date)
+        })
+    }
   return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
-  
+
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -47,7 +52,7 @@ export default ({ data: bills, loading, error }) => {
   } else if (error) {
     return ErrorPage(error)
   }
-  
+
   return (`
     <div class='layout'>
       ${VerticalLayout(120)}
