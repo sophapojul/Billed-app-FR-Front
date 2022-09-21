6a08df0a81466674f18f21fc58758a8e4209c7fa
Pascal Allau <pascal.allau@zohomail.eu>
2022-09-21 11:50:47 +0200

    test(__tests__/Bills): add test it should open modal on click on eye icon as employee

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,39 +1,53 @@
-/**
- * @jest-environment jsdom
- */
+import { screen } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
+import BillsUI from '../views/BillsUI';
+import { bills } from '../fixtures/bills';
+import { ROUTES_PATH } from '../constants/routes';
+import { localStorageMock } from '../__mocks__/localStorage';
 
-import {screen, waitFor} from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
+import router from '../app/Router';
+import Bills from '../containers/Bills';
 
-import router from "../app/Router.js";
+const onNavigate = (pathname) => {
+  document.body.innerHTML = ROUTES_PATH({ pathname });
+};
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", async () => {
-
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+describe('Given I am connected as an employee', () => {
+  describe('When I am on Bills Page', () => {
+    test('Then bill icon in vertical layout should be highlighted', async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock });
       window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
-      await waitFor(() => screen.getByTestId('icon-window'))
-      const windowIcon = screen.getByTestId('icon-window')
-      // TODO write expect expression
-
-    })
-    test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
-  })
-})
+        type: 'Employee',
+      }));
+      const root = document.createElement('div');
+      root.setAttribute('id', 'root');
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      await screen.findByTestId('icon-window');
+      const windowIcon = screen.getByTestId('icon-window');
+      expect(windowIcon).toHaveClass('active-icon');
+    });
+    test('Then bills should be ordered from earliest to latest', () => {
+      document.body.innerHTML = BillsUI({ data: bills });
+      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map((a) => a.innerHTML);
+      const antiChrono = (a, b) => ((a < b) ? 1 : -1);
+      const datesSorted = [...dates].sort(antiChrono);
+      expect(dates).toEqual(datesSorted);
+    });
+    it('should open modal on click', () => {
+      document.body.innerHTML = BillsUI({ data: bills });
+      const bill = new Bills({
+        document,
+        onNavigate,
+        firestore: null,
+        localStorage: window.localStorage,
+      });
+      const iconEye = screen.getAllByTestId('icon-eye')[0];
+      const handleClickIconEye = jest.fn(bill.handleClickIconEye(iconEye));
+      iconEye.addEventListener('click', handleClickIconEye);
+      userEvent.click(iconEye);
+      expect(handleClickIconEye).toHaveBeenCalled();
+    });
+  });
+});
