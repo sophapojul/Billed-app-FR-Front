868aa5aea5ae2c87952b9d54807916dcea07e56d
Pascal Allau <pascal.allau@zohomail.eu>
2022-09-21 11:18:29 +0200

    test(src/views/BillsUI):fix bug when data is undefined

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,10 +20,12 @@ const row = (bill) => {
 }
 
 const rows = (data) => {
-    const sortedData = data.sort((a, b) => {
-        return new Date(b.date) - new Date(a.date)
-    });
-    return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+    if (data && data.length) {
+        const sortedData = data.sort((a, b) => {
+            return new Date(b.date) - new Date(a.date)
+        });
+            return (data && data.length) ? sortedData.map(bill => row(bill)).join("") : ""
+    }
 }
 
 export default ({data: bills, loading, error}) => {
