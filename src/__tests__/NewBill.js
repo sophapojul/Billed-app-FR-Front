3942a0f02533f29d706d41ecf82f03fc6024e4e9
Pascal Allau <pascal.allau@zohomail.eu>
2022-11-21 17:57:30 +0100

    test handleSubmit updateBill apart catch(error)

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -62,5 +62,52 @@ describe('Given I am connected as an employee', () => {
       userEvent.upload(input, file);
       expect(handleChangeFile).toHaveBeenCalledTimes(2);
     });
+    it('should add new bill when I fill correctly fields and I submit', async () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store: storeMock,
+        localStorage: window.localStorage,
+      });
+      const handleSubmit = jest.fn(newBill.handleSubmit);
+      const form = screen.getByTestId('form-new-bill');
+      form.addEventListener('submit', handleSubmit);
+      const uint8 = new Uint8Array([0xff, 0xd8, 0xff]); // JPEG signature
+      const file = new File([uint8], 'test.jpeg', { type: 'image/jpeg' });
+      const input = screen.getByTestId('file');
+      input.addEventListener('change', newBill.handleChangeFile);
+      fireEvent.change(input, {
+        target: {
+          files: [file],
+        },
+      });
+      fireEvent.change(screen.getByTestId('expense-name'), {
+        target: { value: 'test' },
+      });
+      fireEvent.change(screen.getByTestId('datepicker'), {
+        target: { value: '2021-03-01' },
+      });
+      fireEvent.change(screen.getByTestId('amount'), {
+        target: { value: '100' },
+      });
+      fireEvent.change(screen.getByTestId('vat'), {
+        target: { value: '20' },
+      });
+      fireEvent.change(screen.getByTestId('pct'), {
+        target: { value: '20' },
+      });
+      fireEvent.change(screen.getByTestId('commentary'), {
+        target: { value: 'test' },
+      });
+      newBill.fileName = 'test.jpeg';
+      newBill.fileUrl = 'http://localhost:3000/test.jpeg';
+      const submitButton = screen.getByText('Envoyer');
+      submitButton.addEventListener('click', handleSubmit);
+      fireEvent.submit(form);
+      expect(handleSubmit).toBeCalledTimes(1);
+    });
   });
 });
