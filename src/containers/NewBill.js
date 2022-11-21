e8ad88866d529f534bd6a6db4c08b35b343415be
Pascal Allau <pascal.allau@zohomail.eu>
2022-11-21 18:27:04 +0100

    add if statement to check file mime type to allow only png, jpg or jpeg image

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -23,51 +23,31 @@ export default class NewBill {
     const file = this.document.querySelector('input[data-testid="file"]').files[0];
     const filePath = e.target.value.split(/\\/g);
     const fileName = filePath[filePath.length - 1];
-    // test mime type
-    const fileReader = new FileReader();
-    fileReader.onloadend = (event) => {
-      const arr = (new Uint8Array(event.target.result)).subarray(0, 3);
-      const header = arr.reduce((acc, item) => acc + item.toString(16), '');
-      let type = '';
-      // check the magic numbers https://en.wikipedia.org/wiki/List_of_file_signatures
-      switch (header) {
-        case '89504e':
-          type = 'image/png';
-        case 'ffd8ff':
-          type = 'image/jpeg';
-          break;
-        default:
-          type = 'unknown';
-          break;
-      }
-      if (type !== 'image/jpeg' && type !== 'image/png') {
-        document.querySelector('input[data-testid="file"]').value = '';
-        document.querySelector('input[data-testid="file"]').style.backgroundColor = '#f8d7da';
-        alert('Le fichier doit être au format jpeg, jpg ou png');
-      } else {
-        this.fileName = fileName;
-      }
-    };
-    fileReader.readAsArrayBuffer(file);
-    const formData = new FormData();
-    const { email } = JSON.parse(localStorage.getItem('user'));
-    formData.append('file', file);
-    formData.append('email', email);
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true,
-        },
-      })
-      .then(({ fileUrl, key }) => {
-        console.log(this);
-        console.log('fileUrl', fileUrl);
-        this.billId = key;
-        this.fileUrl = fileUrl;
-        this.fileName = fileName;
-      }).catch((error) => console.error(error));
+    // check mime type file
+    const fileTypesEnabled = ['image/png', 'image/jpg', 'image/jpeg'];
+    if (!fileTypesEnabled.includes(file.type)) {
+      alert('Format de fichier non supporté, le fichier doit être au format jpeg, jpg ou png');
+      this.document.querySelector('input[data-testid="file"]').value = '';
+    } else {
+      const formData = new FormData();
+      const { email } = JSON.parse(localStorage.getItem('user'));
+      formData.append('file', file);
+      formData.append('email', email);
+      this.store
+        .bills()
+        .create({
+          data: formData,
+          headers: {
+            noContentType: true,
+          },
+        })
+        .then(({ fileUrl, key }) => {
+          console.log('fileUrl', fileUrl);
+          this.billId = key;
+          this.fileUrl = fileUrl;
+          this.fileName = fileName;
+        }).catch((error) => console.error(error));
+    }
   };
 
   handleSubmit = (e) => {
@@ -78,10 +58,10 @@ export default class NewBill {
       email,
       type: e.target.querySelector('select[data-testid="expense-type"]').value,
       name: e.target.querySelector('input[data-testid="expense-name"]').value,
-      amount: parseInt(e.target.querySelector('input[data-testid="amount"]').value),
+      amount: parseInt(e.target.querySelector('input[data-testid="amount"]').value, 10),
       date: e.target.querySelector('input[data-testid="datepicker"]').value,
       vat: e.target.querySelector('input[data-testid="vat"]').value,
-      pct: parseInt(e.target.querySelector('input[data-testid="pct"]').value) || 20,
+      pct: parseInt(e.target.querySelector('input[data-testid="pct"]').value, 10) || 20,
       commentary: e.target.querySelector('textarea[data-testid="commentary"]').value,
       fileUrl: this.fileUrl,
       fileName: this.fileName,
